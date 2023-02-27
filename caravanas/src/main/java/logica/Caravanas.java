c3235a5fb73e173327313fe048453fe491181b40
puntit0 <elias2016acosta@gmail.com>
2023-02-27 00:00:35 -0300

    relocalizacion del proyecto

---
+++ caravanas/src/main/java/logica/Caravanas.java
@@ -0,0 +1,18 @@
+
+package logica;
+
+import igu.InterfaceApp;
+
+/**
+ *
+ * @author elias
+ */
+public class Caravanas {
+
+    public static void main(String[] args) {
+        
+        InterfaceApp pantalla = new InterfaceApp();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
